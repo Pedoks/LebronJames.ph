41d9576426f9dfb88fedf077245018c58f976903
Rexx012 <alfredbelles04@gmail.com>
2025-06-11 23:19:29 +0800

    Fix config path

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const connectDB = require('./config/db');
+const connectDB = require('./Config/db');
 const userRoutes = require('./routes/userRoutes');
 const articleRoutes = require('./routes/articleRoutes');
 
